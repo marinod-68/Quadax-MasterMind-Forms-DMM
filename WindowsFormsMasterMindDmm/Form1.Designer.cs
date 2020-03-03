cc7d0adabd6a849419f001403f1cdd30634a5620
Don Marino <B520007@federated.fds>
2020-03-03 12:07:18 -0500

    Initial commit for mastermind forms project

---
+++ WindowsFormsMasterMindDmm/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace WindowsFormsMasterMindDmm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelInstructions = new System.Windows.Forms.Label();
+            this.labelFinalOutcome = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.labelAttemptsLeft = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(176, 109);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(38, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(230, 109);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(38, 20);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(283, 109);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(38, 20);
+            this.numericUpDown3.TabIndex = 4;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(338, 109);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(38, 20);
+            this.numericUpDown4.TabIndex = 5;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(208, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Lock-in Combination";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(233, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(286, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(341, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // labelInstructions
+            // 
+            this.labelInstructions.AutoSize = true;
+            this.labelInstructions.Location = new System.Drawing.Point(98, 74);
+            this.labelInstructions.Name = "labelInstructions";
+            this.labelInstructions.Size = new System.Drawing.Size(407, 13);
+            this.labelInstructions.TabIndex = 12;
+            this.labelInstructions.Text = "Please select a combination of 4 digits between 1 and 6 (inclusive) - Max 10 atte" +
+                "mpts";
+            this.labelInstructions.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelFinalOutcome
+            // 
+            this.labelFinalOutcome.AutoSize = true;
+            this.labelFinalOutcome.Location = new System.Drawing.Point(173, 306);
+            this.labelFinalOutcome.Name = "labelFinalOutcome";
+            this.labelFinalOutcome.Size = new System.Drawing.Size(0, 13);
+            this.labelFinalOutcome.TabIndex = 13;
+            this.labelFinalOutcome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelFinalOutcome.Visible = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(519, 109);
+            this.progressBar1.Maximum = 10;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(102, 14);
+            this.progressBar1.TabIndex = 14;
+            // 
+            // labelAttemptsLeft
+            // 
+            this.labelAttemptsLeft.AutoSize = true;
+            this.labelAttemptsLeft.Location = new System.Drawing.Point(521, 95);
+            this.labelAttemptsLeft.Name = "labelAttemptsLeft";
+            this.labelAttemptsLeft.Size = new System.Drawing.Size(35, 13);
+            this.labelAttemptsLeft.TabIndex = 15;
+            this.labelAttemptsLeft.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 383);
+            this.Controls.Add(this.labelAttemptsLeft);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.labelFinalOutcome);
+            this.Controls.Add(this.labelInstructions);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelInstructions;
+        private System.Windows.Forms.Label labelFinalOutcome;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label labelAttemptsLeft;
+    }
+}
+
---
+++ WindowsFormsMasterMindDmm/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsMasterMindDmm
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+
+        int attempts = 0;
+        int[] answer = new int[4];
+        int[] guess = new int[4];
+        char[] answerKey = new char[4];
+        char[] guessKey = new char[4];
+
+        const int MAX_ATTEMPTS = 10;
+        const char A_SPACE = ' ';
+        const char PLUS = '+';
+        const char MINUS = '-';
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            answer[0] = rnd.Next(1, 6);
+            answer[1] = rnd.Next(1, 6);
+            answer[2] = rnd.Next(1, 6);
+            answer[3] = rnd.Next(1, 6);
+            progressBar1.Value = MAX_ATTEMPTS;
+            labelAttemptsLeft.Text = String.Format("{0} Attempts left", MAX_ATTEMPTS);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            guess[0] = (int)numericUpDown1.Value;
+            guess[1] = (int)numericUpDown2.Value;
+            guess[2] = (int)numericUpDown3.Value;
+            guess[3] = (int)numericUpDown4.Value;
+
+            answerKey[0] = answerKey[1] = answerKey[2] = answerKey[3] = guessKey[0] = guessKey[1] = guessKey[2] = guessKey[3] = A_SPACE;
+
+            if (compareAnswerAndGuess())
+            {
+
+                //labelFinalOutcome.Visible = true;
+                //labelFinalOutcome.Text = String.Format("Combination found, you are a BIG WINNER!!!");
+
+                MessageBox.Show("Combination found, you are a BIG WINNER!!!", "", MessageBoxButtons.OK);
+                Application.Exit();
+            }
+            else
+            {
+                ++attempts;
+                labelAttemptsLeft.Text = String.Format("{0} Attempts left", MAX_ATTEMPTS - attempts);
+                progressBar1.Value = MAX_ATTEMPTS - attempts;
+
+                switch (guessKey[0])
+                {
+                    case A_SPACE:
+                        label1.Visible = false;
+                        break;
+                    case PLUS:
+                        label1.Visible = true;
+                        label1.Text = "+";
+                        break;
+                    case MINUS:
+                        label1.Visible = true;
+                        label1.Text = "-";
+                        break;
+                }
+                switch (guessKey[1])
+                {
+                    case A_SPACE:
+                        label2.Visible = false;
+                        break;
+                    case PLUS:
+                        label2.Visible = true;
+                        label2.Text = "+";
+                        break;
+                    case MINUS:
+                        label2.Visible = true;
+                        label2.Text = "-";
+                        break;
+                }
+                switch (guessKey[2])
+                {
+                    case A_SPACE:
+                        label3.Visible = false;
+                        break;
+                    case PLUS:
+                        label3.Visible = true;
+                        label3.Text = "+";
+                        break;
+                    case MINUS:
+                        label3.Visible = true;
+                        label3.Text = "-";
+                        break;
+                } //end switch (guessKey[2]
+                switch (guessKey[3])
+                {
+                    case A_SPACE:
+                        label4.Visible = false;
+                        break;
+                    case PLUS:
+                        label4.Visible = true;
+                        label4.Text = "+";
+                        break;
+                    case MINUS:
+                        label4.Visible = true;
+                        label4.Text = "-";
+                        break;
+                }// end switch (guessKey[3])
+            } //end else
+
+            if (attempts > 9)
+            {
+                String msg = String.Format("You've lost, your guesses have expired.  The combination was {0} {1} {2} {3}", answer[0], answer[1], answer[2], answer[3]);
+                MessageBox.Show(msg, "", MessageBoxButtons.OK);
+                Application.Exit();
+            }
+
+        } //end private void button1_Click(object sender, EventArgs e)
+
+        private bool compareAnswerAndGuess()
+        {
+            try
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (guess[i] == answer[i])
+                        answerKey[i] = guessKey[i] = PLUS;
+                } //end for (int i = 0; i < 4; i++)
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (guessKey[i] == PLUS)
+                        continue;
+
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if ((answerKey[j] != PLUS) && (answerKey[j] != MINUS))
+                        {
+                            if (guess[i] == answer[j])
+                            {
+                                guessKey[i] = MINUS;
+                                answerKey[j] = MINUS;
+                                break;
+                            } //end if (guess[i] == answer[j])
+                        } //end if ((answerKey[j] != PLUS) && (answerKey[j] != MINUS))
+                    } //end for (int j = 0; j < 4; j++)
+                } //end for (int i = 0; i < 4; i++)
+
+                return answer.SequenceEqual(guess);
+            } //end try
+            catch (Exception e)
+            {
+                throw e;
+            } //end catch (Exception e)
+        }
+    }
+}
